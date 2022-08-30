85f9f4a7d358bfb13f8a19700ad950a24679c777
Chitrang <chitrang.patel@ncoregames.com>
2022-08-30 15:20:59 +0530

    102-loading-paths-dynamically

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,46 @@
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
+
 export default function Home(props) {
   const { products } = props;
 
   return (
     <ul>
       {products.map((p) => (
-        <li key={p.id}>{p.name}</li>
+        <li key={p.id}>
+          <Link href={{ pathname: `product/${p.id}` }}>{p.name}</Link>
+        </li>
       ))}
     </ul>
   );
 }
 
 export async function getStaticProps(context) {
+  const filePath = path.join(process.cwd(), "data", "dummy-data.json");
+
+  const file = await fs.readFile(filePath);
+
+  const data = JSON.parse(file);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data-path",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: {
-      "products": [
-        { id: "00", name: "product-00" },
-        { id: "01", name: "product-01" },
-        { id: "02", name: "product-02" },
-      ],
+      products: data.products,
     },
+    revalidate: 10,
   };
 }
---
+++ pages/product/[id].js
@@ -0,0 +1,59 @@
+import fs from "fs/promises";
+import path from "path";
+import { Fragment } from "react";
+
+function ProductPage(params) {
+  const { product } = params;
+
+  //if fallback: true
+  // if (!product) {
+  //   return <p>Loading...</p>;
+  // }
+
+  return (
+    <Fragment>
+      <h1>{product.name}</h1>
+    </Fragment>
+  );
+}
+
+export default ProductPage;
+
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-data.json");
+
+  const file = await fs.readFile(filePath);
+
+  const data = JSON.parse(file);
+
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productID = params.id;
+
+  const data = await getData();
+
+  const product = data.products.find((p) => p.id === productID);
+
+  return {
+    props: {
+      product: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((p) => p.id);
+
+  const params = ids.map((_id) => ({ params: { id: _id } }));
+
+  return {
+    paths: params,
+    fallback: false,
+  };
+}
